838ed73e809342d6dc99a1d296d7e1a30616758d
Hallurs <hallurh@7und.is>
2021-01-19 11:42:27 +0000

    Create verkefni3.py

---
+++ verkefni3.py
@@ -0,0 +1,29 @@
+class pizza:
+    pizzas = {}
+    c = 0
+    pizza_list = []
+    def __init__(self, topping1 = None, topping2 = None, topping3 = None):
+        self.pizzas = {self.c: (topping1, topping2, topping3, 'unserved')}
+        for i in self.pizzas:
+            self.pizza_list.append([])
+            self.pizza_list[i].append(str(i))
+            for b in self.pizzas[i]:
+                self.pizza_list[i].append(b)
+        
+
+    def make_served(self, ID):
+        self.pizzas[ID][3] = 'served'
+    
+    def __str__(self):
+        new_list = []
+        for i in self.pizza_list:
+            for b in i:
+                new_list.append(b)
+            new_list.append('\n')
+        return ' '.join(new_list)
+    
+for i in range(3):       
+    pizza1 = pizza('pepperoni', 'ham', 'extra cheese')
+    self.c += 1
+print(pizza1)
+
