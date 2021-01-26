28b4dd452727e9e8ae67754529ab6ae53cd660da
trudzer <trudzeredits@gmail.com>
2021-01-26 13:50:28 +0000

    Add files via upload

--- recursion_part_2.py
+++ recursion_part_2.py
@@ -1,5 +1,4 @@
 def string_recur(word):
-
     if word == "":
         return 0
     return string_recur(word[:-1]) + 1
@@ -26,14 +25,16 @@ def count_instance(list, value):
 
 
 def duplicate(list):
-    for i in list[:-1]:
-        if list[-1] == i:
-            return True
-
-    if len(list) == 0:
+    if list[::] == []:
         return False
 
-    return duplicate(list[:-1])
+    head = list[0]
+    tail = list[1:]
+
+    if linear_search(tail, head):
+        return True
+
+    return duplicate(list[1:])
 
 
 def remove_duplicate(list):
@@ -71,6 +72,6 @@ def binary_search(list, value):
 print("Length of string:",end=" "), print(string_recur('abcdefghijklmn'))
 print("Value in list:", end=" "), print(linear_search([1,2,3,4,5,"hallo",6,7,9], "hallo"))
 print("Number of instances:", end=" "), print(count_instance([1,2,3,4,5,1,2,1,3,4,5,1,2,1,1,1],1))
-print("Duplicates:", end=" "), print(duplicate([65,66,23,11,54,33,65,1,22,57]))
+print("Duplicates:", end=" "), print(duplicate([1,2,3,4,5,6,4,7]))
 print("list without duplicate:", end=" "), print(remove_duplicate([1,2,3,4,1,2,5,6,7,5,6,3,8,9,10,11,11,4]))
 print("Binary search:", end=" "), print(binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], 10))
