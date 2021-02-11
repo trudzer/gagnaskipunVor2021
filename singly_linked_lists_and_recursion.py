dbe7a2cd631ada42df9bcdce458fc7eeb4b802d4
trudzer <trudzeredits@gmail.com>
2021-02-11 21:48:31 +0000

    Create singly_linked_lists_and_recursion.py

---
+++ singly_linked_lists_and_recursion.py
@@ -0,0 +1,129 @@
+class Node:
+
+    def __init__(self,data = None, next = None):
+        self.data = data
+        self.next = next
+
+def print_nodes(head):
+    node = head
+    while node != None:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+def counter_iterative(head):
+    node = head
+    counter = 0
+    while node != None:
+        counter += 1
+        node = node.next
+    return counter
+
+def print_recursive(head):
+    node = head
+    if node == None:
+        return None
+    else:
+        print(node.data, end=" ")
+    return print_recursive(node.next)
+
+def counter_recursive(head):
+    node = head
+    counter = 0
+    return counter_helper(node, counter)
+
+def counter_helper(node, counter):
+    if node == None:
+        return counter
+    counter += 1
+    return counter_helper(node.next, counter)
+
+def linear_search(head, value):
+        if head == None:
+            return False
+        if value == head.data:
+            return True
+        return linear_search(head.next, value)
+
+def delete_every_other(head):
+    return deo_helper(head, 0)
+
+def deo_helper(head, i):
+    if head == None:
+        return None
+    if i % 2 == 1:
+        head.next = deo_helper(head.next, i+1)
+        return head
+    else:
+        return deo_helper(head.next, i+1)
+
+def merge_list(head1, head2):
+    if head1 == None:
+        return head2
+    if head2 == None:
+        return head1
+    if head1.data <= head2.data:
+        head1.next = merge_list(head1.next, head2)
+        return head1
+    else:
+        head2.next = merge_list(head1, head2.next)
+        return head2
+
+def insert_list(head, value):
+    node = head
+    if node == None:
+        node.next = value
+        return node.data
+    if value >= node.data:
+        node.next = value
+        node.next = merge_list(node.next, value)
+        return node
+
+head = Node(1, Node(3, Node(5, Node(7, Node(9, Node(11, Node(13, None)))))))
+head2 = Node(2, Node(4, Node(6, Node(8, Node(10, Node(12, Node(14, None)))))))
+head3 = Node(111, Node(113, Node(115, Node(117, Node(119, Node(1111, Node(1113, None)))))))
+
+insert_list(head, 4)
+print_nodes(head)
+
+print("iterative counter: ", end="")
+print(counter_iterative(head))
+
+print()
+print("recursive counter: ", end="")
+print(counter_recursive(head))
+
+print()
+print("recursive: ", end="")
+print_recursive(head)
+
+print()
+print()
+print("iterative: ", end="")
+print_nodes(head)
+
+print()
+print("iterative: ", end="")
+print_nodes(head2)
+
+print()
+print("linear search: ", end="")
+print(linear_search(head, 3))
+
+print()
+print("iterative: ", end="")
+print_nodes(head3)
+
+print()
+print("delete every other: ", end="")
+print_nodes(delete_every_other(head3))
+
+lis = merge_list(head,head2)
+print()
+print("merge list: ", end="")
+print_nodes(lis)
+
+print()
+print("merge list: ", end="")
+lis = merge_list(lis, head3)
+print_nodes(lis)
