d62895e76afa4a4c337c54e24beb3f569dba6c46
trudzer <trudzeredits@gmail.com>
2021-04-12 18:19:51 +0000

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -4,8 +4,7 @@ ID = 6
 def clear():
     return print("\n" * 30)
 
-class Pizza:
-    
+class Pizza:  
     def __init__(self,id=0, topping1="", topping2="", topping3="", status="unserved"):
         id_list = []
         self.topping1 = topping1
@@ -21,7 +20,6 @@ class Pizza:
         return pizza_list
 
 class Order:
-
     def __init__(self, pizza_dict):
         self.pizza_dict = pizza_dict
 
@@ -75,7 +73,6 @@ class Order:
                 print("{}. {:<8} --- Pizza with {}".format(id, pizza_dict[id][3], pizza_dict[id][0]))
 
 def main():
-
     pizza1 = Pizza(1, "pepperoni", "cheese")
     pizza2 = Pizza(2, "ham", "bacon", "chicken")
     pizza3 = Pizza(3, "extra-cheese")
